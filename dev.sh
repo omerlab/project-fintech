b48dc03fbed1b1c6f31ca73e7c3dfc70ecf0f2ff
omerlab <blessing.shopping14@gmail.com>
2022-03-03 08:24:00 -0500

    developer credentials

---
+++ dev.sh
@@ -0,0 +1,6 @@
+echo "Welcome to Fintech project to all developper"
+echo "enter your name"
+read name
+echo "enter your pin"
+read pin
+echo "congratulation $name, $pin is a correct entry you can procceed"
